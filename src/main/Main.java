b5aed0e6d4e225eef128c6bc383b69147d4cf25d
Lazar Tsarev <lazar.tsarev@bul-si.bg>
2014-02-04 17:58:56 +0200

    works for certain strings like banana and peeper but not for long one when the tree has to be in more than 2-3 levels depth.

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,41 +4,55 @@ import java.util.Map.Entry;
 
 public class Main {
 	public static void main(String[] args) {
-		Node head = buildTree("peeper");
-		for (Entry<String, Node> entry : head.children.entrySet()) {
-			System.out.println(entry.getKey());
-			Node node = entry.getValue();
-			for (Entry<String, Node> nestedEnrty : node.children.entrySet()) {
-				System.out.println(" - " + nestedEnrty.getKey());
-			}
+		String stringForTree = "ababbabbaabbabb#";
+		Node head = buildTree(stringForTree);
 
-			System.out.println("new general node \n");
+		int depth = 0;
+		printNode(head, depth);
+	}
+	
+	public static void printNode(Node node, int depth) {
+		for (Entry<String, Node> entry : node.children.entrySet()) {
+			for(int i = 0; depth - i >= 0; i++) {
+				System.out.print('-');
+			}
+			System.out.println(entry.getKey());
+			printNode(entry.getValue(), depth + 1);
 		}
 	}
 
 	public static Node buildTree(String string) {
 		Node head = new Node();
-		for (int i = 0; i < string.length(); i++) {
+		for (int i = 0; i < string.length() - 1; i++) {
 			insertNode(head, string.substring(i));
 		}
 		return head;
 	}
 
 	public static void insertNode(Node head, String subStr) {
-		String containedKey = null;
+		String key = null;
 		for (Entry<String, Node> entry : head.children.entrySet()) {
-			String key = entry.getKey();
-			if (subStr.charAt(0) == key.charAt(0)) {
-				containedKey = key;
+			String currentKey = entry.getKey();
+			if (subStr.charAt(0) == currentKey.charAt(0)) {
+				key = currentKey;
 				break;
-				
 			}
 		}
 
-		if (containedKey == null) {
+		if (key == null) {
 			head.children.put(subStr, new Node());
 		} else {
-			insertFoundNode(head, containedKey, subStr);
+			int matchingCharsCnt = findMatchingCharsCnt(subStr, key);
+			
+			if(matchingCharsCnt == key.length() && matchingCharsCnt == subStr.length()) { // daljinata na savpada6tiq string e kolkoto daljinata na string, koito trqbva da se dobavi
+				
+			} else if(matchingCharsCnt == key.length()) { // daljinata na savda6tiq string e kolkoto daljinata na kliu4a na node-a
+				
+			} else if(matchingCharsCnt == subStr.length()) { // daljinata na savda6tiq string e kolkoto daljinata na string koito procesvame
+				
+			}
+			
+			insertFoundNode(head, key, subStr);
 		}
 	}
 	
@@ -48,7 +62,7 @@ public class Main {
 		Node node = head.children.get(key);
 		if (matchingCharsCnt == key.length()) {
 			// if there are already elements that begins with this string then we just add new node
-			node.children.put(subStr.substring(1), new Node());
+			node.children.put(subStr.substring(matchingCharsCnt), new Node());
 		} else {
 			// delete the old node
 			head.children.remove(key);
