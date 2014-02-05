6fc94f70094cd5e657a0b2e79012203d83c6c48f
Lazar Tsarev <lazar.tsarev@bul-si.bg>
2014-02-05 16:06:42 +0200

    fixed building the tree and finding the longest recurring cycle ( matchingCharsCnt didn't break when there was mismatch and later in the strings matches were found )

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,48 @@
 package main;
 
+import java.util.Arrays;
 import java.util.Map.Entry;
 
-public class Main {
-	public static void main(String[] args) {
-		String stringForTree = "ababbabbaabbabb#";
-		Node head = buildTree(stringForTree);
+import sun.beans.editors.LongEditor;
 
+public class Main {
+	public static String longestString = "";
+	
+	public static void main(String[] args) {
+//		String stringForTree = "ababbabbaabbabb";
+//		String stringForTree = "peeper";
+//		String stringForTree = "banana";
+//		String stringForTree = "caracas";
+		String stringForTree = "020408163265306122448979591836734693877551020408163265306122448979591836734693877551";
+		long start = System.currentTimeMillis();
+		Node head = buildTree(stringForTree);
+		System.out.println("Used time is for building is: " + (System.currentTimeMillis() - start) + " ms");
+		
 		int depth = 0;
 		printNode(head, depth);
+		
+		findTheLongestRepeatingString(head);
+		System.out.println("\n\nAaaaaand the longest string is : " + longestString);
+	}
+	
+	public static void findTheLongestRepeatingString(Node head) {
+		String string = "";
+		for(Entry<String, Node> entry : head.children.entrySet()) {
+			recur(entry.getValue(), entry.getKey(), string);
+		}
+	}
+	
+	public static void recur(Node node, String nodeKey, String string) {
+		string = string + nodeKey;
+		if(node.children.size() >= 2) {
+			if(string.length() > longestString.length()) {
+				longestString = string;
+			}
+		}
+		
+		for(Entry<String, Node> entry : node.children.entrySet()) {
+			recur(entry.getValue(), entry.getKey(), string);
+		}
 	}
 	
 	public static void printNode(Node node, int depth) {
@@ -23,63 +57,68 @@ public class Main {
 
 	public static Node buildTree(String string) {
 		Node head = new Node();
-		for (int i = 0; i < string.length() - 1; i++) {
+		for (int i = 0; i < string.length(); i++) {
 			insertNode(head, string.substring(i));
 		}
 		return head;
 	}
 
-	public static void insertNode(Node head, String subStr) {
-		String key = null;
-		for (Entry<String, Node> entry : head.children.entrySet()) {
+	public static void insertNode(Node parent, String newKey) {
+//		System.out.println("търсим ключ за : " + newKey);
+		String key = "";
+		for (Entry<String, Node> entry : parent.children.entrySet()) {
 			String currentKey = entry.getKey();
-			if (subStr.charAt(0) == currentKey.charAt(0)) {
+			if (newKey.charAt(0) == currentKey.charAt(0)) {
 				key = currentKey;
 				break;
 			}
 		}
 
-		if (key == null) {
-			head.children.put(subStr, new Node());
-		} else {
-			int matchingCharsCnt = findMatchingCharsCnt(subStr, key);
-			
-			if(matchingCharsCnt == key.length() && matchingCharsCnt == subStr.length()) { // daljinata na savpada6tiq string e kolkoto daljinata na string, koito trqbva da se dobavi
-				
-			} else if(matchingCharsCnt == key.length()) { // daljinata na savda6tiq string e kolkoto daljinata na kliu4a na node-a
-				
-			} else if(matchingCharsCnt == subStr.length()) { // daljinata na savda6tiq string e kolkoto daljinata na string koito procesvame
-				
+		int matchingCharsCnt = findMatchingCharsCnt(newKey, key);
+		
+		Node node = parent.getNode(key);
+		if(matchingCharsCnt == 0) {
+//			System.out.println("няма съвпадащ ключ за : " + newKey);
+			parent.addNode(newKey, new Node());
+		} 
+		else {
+//			System.out.println("има намерен ключ за : " + newKey);
+//			System.out.println("като ето и дължините : newKey(" + newKey.length() + ") = " + newKey + ", key(" + key.length() + ") = " + key + ", matchingCharsCnt = " + matchingCharsCnt);
+		
+			if(matchingCharsCnt == key.length() && matchingCharsCnt == newKey.length()) {
+				node.addNode("#", new Node());
+			} 
+			else 
+			if(matchingCharsCnt == key.length()) {
+				insertNode(node, newKey.substring(matchingCharsCnt));
+			} 
+			else 
+			if(matchingCharsCnt == newKey.length()) {
+				explodeNodeAndAddChild(parent, key, newKey, matchingCharsCnt);
 			}
-			
-			insertFoundNode(head, key, subStr);
+			else {
+				explodeNodeAndAddChild(parent, key, newKey, matchingCharsCnt);
+			}
 		}
+//		System.out.println("\n");
 	}
 	
-	public static void insertFoundNode(Node head, String key, String subStr) {
-		int matchingCharsCnt = findMatchingCharsCnt(subStr, key);
+	public static void explodeNodeAndAddChild(Node parent, String toBeExplodedKey, String toBeAddedKey, int matchingCharsCnt) {
+		Node toBeExploded = parent.getNode(toBeExplodedKey);
 		
-		Node node = head.children.get(key);
-		if (matchingCharsCnt == key.length()) {
-			// if there are already elements that begins with this string then we just add new node
-			node.children.put(subStr.substring(matchingCharsCnt), new Node());
+		String newNodeKey = toBeExplodedKey.substring(0, matchingCharsCnt);
+		
+		Node newNode = new Node();
+		newNode.addNode(toBeExplodedKey.substring(matchingCharsCnt), toBeExploded);
+		if(toBeAddedKey.length() == matchingCharsCnt) {
+			newNode.addNode("#", new Node());
 		} else {
-			// delete the old node
-			head.children.remove(key);
-
-			// remove one char from the key
-			String reducedKey = key.substring(matchingCharsCnt);
-
-			Node newNode = new Node();
-			// insert the old element in the new one
-			newNode.children.put(reducedKey, node);
-
-			// insert the new element in the node
-			newNode.children.put(subStr.substring(matchingCharsCnt), new Node());
-
-			// insert the new node in the tree
-			head.children.put(subStr.substring(0, matchingCharsCnt), newNode);
+			newNode.addNode(toBeAddedKey.substring(matchingCharsCnt), new Node());
 		}
+		
+		
+		parent.removeNode(toBeExplodedKey);
+		parent.addNode(newNodeKey, newNode);
 	}
 	
 	public static int findMatchingCharsCnt(String first, String second) {
@@ -87,6 +126,8 @@ public class Main {
 		for(int i = 0; i < first.length() && i < second.length(); i++) {
 			if(first.charAt(i) == second.charAt(i)) {
 				matchingCharsCnt++;
+			} else {
+				break;
 			}
 		}
 		return matchingCharsCnt;
--- src/main/Node.java
+++ src/main/Node.java
@@ -8,4 +8,16 @@ public class Node {
 	}
 	
 	public HashMap<String, Node> children;
+	
+	public Node getNode(String key) {
+		return children.get(key);
+	}
+	
+	public void addNode(String key, Node node) {
+		children.put(key, node);
+	}
+
+	public void removeNode(String key) {
+		children.remove(key);
+	}
 }
